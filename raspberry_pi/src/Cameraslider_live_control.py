5eb379680c53b99889f05cc481253340f6ace68e
nmarkhorst <niels.markhorst@altran.com>
2017-09-04 13:41:37 +0200

    live test

--- raspberry_pi/src/Cameraslider_live_control.py
+++ raspberry_pi/src/Cameraslider_live_control.py
@@ -44,44 +44,40 @@ try:
     while True:
     # Keyboard character retrieval method is called and saved
     # into variable
-        for event in pygame.event.get():
-            if (event.type == KEYDOWN):
-                print("event")
-                down = True
-            # The car will drive forward when the "w" key is pressed
-                if (event.key == K_KP8):
-                    print("up")
-                    GPIO.output(20,GPIO.HIGH)
-                    GPIO.output(TiltStepPin,GPIO.LOW)
-                    time.sleep(0.001)
-                    GPIO.output(TiltStepPin,GPIO.HIGH)
-                    time.sleep(0.001)
-            # The car will reverse when the "s" key is pressed
-                if (event.key == K_KP2):
-                    print("down")
-                    GPIO.output(20,GPIO.LOW)
-                    GPIO.output(TiltStepPin,GPIO.LOW)
-                    time.sleep(0.001)
-                    GPIO.output(TiltStepPin,GPIO.HIGH)
-                    time.sleep(0.001)
+        keys = pygame.key.get_pressed()
+    # The car will drive forward when the "w" key is pressed
+        if keys[K_KP8]:
+            print("up")
+            GPIO.output(20,GPIO.HIGH)
+            GPIO.output(TiltStepPin,GPIO.LOW)
+            time.sleep(0.001)
+            GPIO.output(TiltStepPin,GPIO.HIGH)
+            time.sleep(0.001)
+    # The car will reverse when the "s" key is pressed
+        if keys[K_KP2]:
+            print("down")
+            GPIO.output(20,GPIO.LOW)
+            GPIO.output(TiltStepPin,GPIO.LOW)
+            time.sleep(0.001)
+            GPIO.output(TiltStepPin,GPIO.HIGH)
+            time.sleep(0.001)
+    # The "a" key will toggle the steering left
+        if keys[K_KP4]:
+            print("left")
+            GPIO.output(7,GPIO.LOW)
+            GPIO.output(PanStepPin,GPIO.LOW)
+            time.sleep(0.001)
+            GPIO.output(PanStepPin,GPIO.HIGH)
+            time.sleep(0.001)
 
-            # The "a" key will toggle the steering left
-                if (event.key == K_KP4):
-                    print("left")
-                    GPIO.output(7,GPIO.LOW)
-                    GPIO.output(PanStepPin,GPIO.LOW)
-                    time.sleep(0.001)
-                    GPIO.output(PanStepPin,GPIO.HIGH)
-                    time.sleep(0.001)
-
-            # The "d" key will toggle the steering right
-                if (event.key == K_KP6):
-                    print("right")
-                    GPIO.output(7,GPIO.HIGH)
-                    GPIO.output(PanStepPin,GPIO.LOW)
-                    time.sleep(0.001)
-                    GPIO.output(PanStepPin,GPIO.HIGH)
-                    time.sleep(0.001)
+    # The "d" key will toggle the steering right
+        if keys[K_KP6]:
+            print("right")
+            GPIO.output(7,GPIO.HIGH)
+            GPIO.output(PanStepPin,GPIO.LOW)
+            time.sleep(0.001)
+            GPIO.output(PanStepPin,GPIO.HIGH)
+            time.sleep(0.001)
 
 
 except:

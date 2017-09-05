4312a61968edf041a75bb1bd0ef8692e0d3d1685
Niels Markhorst <mark6028a@gmail.com>
2017-09-05 06:13:12 +0000

    added first draft of live control using keyboard

--- raspberry_pi/src/Cameraslider_live_control.py
+++ raspberry_pi/src/Cameraslider_live_control.py
@@ -5,8 +5,8 @@ import time
 import sys
 import Tkinter as tk
 
-pygame.init()
-pygame.display.set_mode((1,1))
+#pygame.init()
+#pygame.display.set_mode((1,1))
 
 
 SlideStepPin = 23
@@ -38,9 +38,12 @@ GPIO.output(21,GPIO.LOW)
 GPIO.output(22,GPIO.HIGH)
 GPIO.output(11,GPIO.HIGH)
 GPIO.output(19,GPIO.HIGH)
+stepsize = 0
+
 try:
 
     def key(event):
+        global stepsize
         """shows key or tk code for the key"""
         if event.keysym == 'Escape':
             root.destroy()
@@ -49,39 +52,56 @@ try:
             # normal number and letter characters
             print( 'Normal Key %r' % event.char )
             if event.keysym == "a":
-                print("A pressed")
+                GPIO.output(24,GPIO.LOW)
+                for x in range (0,stepsize):
+                    GPIO.output(SlideStepPin,GPIO.LOW)
+                    time.sleep(0.001)
+                    GPIO.output(SlideStepPin,GPIO.HIGH)
+                    time.sleep(0.001)
             elif event.keysym == "d":
-                print("D pressed")
+                GPIO.output(24,GPIO.HIGH)
+                for x in range (0,stepsize):
+                    GPIO.output(SlideStepPin,GPIO.LOW)
+                    time.sleep(0.001)
+                    GPIO.output(SlideStepPin,GPIO.HIGH)
+                    time.sleep(0.001)
         elif len(event.char) == 1:
             # charcters like []/.,><#$ also Return and ctrl/key
             print( 'Punctuation Key %r (%r)' % (event.keysym, event.char) )
         else:
             # f1 to f12, shift keys, caps lock, Home, End, Delete ...
             print( 'Special Key %r' % event.keysym )
+            if "F" in event.keysym:
+                stepsize = int(event.keysym[1:]) * 2
+                print(stepsize)
             if event.keysym == "Down":
-                GPIO.output(20,GPIO.LOW)
-                GPIO.output(TiltStepPin,GPIO.LOW)
-                time.sleep(0.001)
-                GPIO.output(TiltStepPin,GPIO.HIGH)
-                time.sleep(0.001)
-            elif event.keysym == "Up":
                 GPIO.output(20,GPIO.HIGH)
-                GPIO.output(TiltStepPin,GPIO.LOW)
-                time.sleep(0.001)
-                GPIO.output(TiltStepPin,GPIO.HIGH)
-                time.sleep(0.001)
+                for x in range (0,stepsize):
+                    GPIO.output(TiltStepPin,GPIO.LOW)
+                    time.sleep(0.001)
+                    GPIO.output(TiltStepPin,GPIO.HIGH)
+                    time.sleep(0.001)
+            elif event.keysym == "Up":
+                GPIO.output(20,GPIO.LOW)
+                for x in range (0,stepsize):
+                    GPIO.output(TiltStepPin,GPIO.LOW)
+                    time.sleep(0.001)
+                    GPIO.output(TiltStepPin,GPIO.HIGH)
+                    time.sleep(0.001)
             elif event.keysym == "Left":
                 GPIO.output(7,GPIO.LOW)
-                GPIO.output(PanStepPin,GPIO.LOW)
-                time.sleep(0.001)
-                GPIO.output(PanStepPin,GPIO.HIGH)
-                time.sleep(0.001)
+                for x in range (0,stepsize):
+                    GPIO.output(PanStepPin,GPIO.LOW)
+                    time.sleep(0.001)
+                    GPIO.output(PanStepPin,GPIO.HIGH)
+                    time.sleep(0.001)
             elif event.keysym == "Right":
                 GPIO.output(7,GPIO.HIGH)
-                GPIO.output(PanStepPin,GPIO.LOW)
-                time.sleep(0.001)
-                GPIO.output(PanStepPin,GPIO.HIGH)
-                time.sleep(0.001)
+                for x in range (0,stepsize):
+                    GPIO.output(PanStepPin,GPIO.LOW)
+                    time.sleep(0.001)
+                    GPIO.output(PanStepPin,GPIO.HIGH)
+                    time.sleep(0.001)
 
     root = tk.Tk()
     print( "Press a key (Escape key to exit):" )
